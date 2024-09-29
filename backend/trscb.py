bd19b087e18896c7ae6e965ea4cca21b24a98ea6
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2024-09-29 09:47:22 +0530

    refactor

--- backend/trscb.py
+++ backend/trscb.py
@@ -1,6 +1,7 @@
 import logging
+import os
+import shutil
 import subprocess
-from transcribe_anything.api import transcribe
 
 
 def transcribe_audio_whisper(audio_path):
@@ -9,19 +10,19 @@ def transcribe_audio_whisper(audio_path):
     """
     try:
         print("audio path : ", audio_path)
-        # Run the transcribe-anything command
-        # result = subprocess.run(
-        #     ["transcribe-anything", audio_path],
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.STDOUT,
-        #     text=True,
-        # )
         result = subprocess.run(
             [
                 "transcribe-anything",
-                "Why I stopped choking the chicken (mp3cut.net).mp3",
+                audio_path,
             ]
         )
+        transcript_path = "text_" + ".".join(audio_path.split(".")[:-1])
+        transcription = ""
+        with open(os.path.join(transcript_path, "out.txt")) as F:
+            transcription = F.read()
+
+        # THIS DELETES THE FOLDER, COMMENT IT OUT IF YOU WANT TO KEEP IT
+        shutil.rmtree(transcript_path)
 
         print("result :", result)
 
@@ -29,13 +30,15 @@ def transcribe_audio_whisper(audio_path):
             print(f"Error transcribing audio: {result.stderr}")
             return ""
 
-        transcription = result.stdout
+        # transcription = result.stdout
         logging.debug(f"Transcription: {transcription}")
         return transcription
     except Exception as e:
-        print(f"Error transcribing audio: {e}", exc_info=True)
+        print(f"Error transcribing audio: {e}")
         return ""
 
 
 if __name__ == "__main__":
-    transcribe_audio_whisper("Why I stopped choking the chicken.mp3")
+    audio_path = "Why I stopped choking the chicken (mp3cut.net).mp3"
+    x = transcribe_audio_whisper(audio_path=audio_path)
+    print("transcription : ", x)
