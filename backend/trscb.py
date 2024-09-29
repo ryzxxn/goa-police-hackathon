72640f13606fbaedd62f30598b861eefc96a0491
RhysJosmin <55994011+Rhysjosmin@users.noreply.github.com>
2024-09-29 09:50:54 +0530

    made it use api

--- backend/trscb.py
+++ backend/trscb.py
@@ -2,6 +2,7 @@ import logging
 import os
 import shutil
 import subprocess
+from transcribe_anything.api import transcribe
 
 
 def transcribe_audio_whisper(audio_path):
@@ -10,13 +11,18 @@ def transcribe_audio_whisper(audio_path):
     """
     try:
         print("audio path : ", audio_path)
-        result = subprocess.run(
-            [
-                "transcribe-anything",
-                audio_path,
-            ]
+        # result = subprocess.run(
+        #     [
+        #         "transcribe-anything",
+        #         audio_path,
+        #     ]
+        # )
+        result = transcribe(
+            url_or_file=audio_path,
+            output_dir="output_dir",
         )
-        transcript_path = "text_" + ".".join(audio_path.split(".")[:-1])
+        print("result :", result)
+        transcript_path = "output_dir"
         transcription = ""
         with open(os.path.join(transcript_path, "out.txt")) as F:
             transcription = F.read()
@@ -24,7 +30,6 @@ def transcribe_audio_whisper(audio_path):
         # THIS DELETES THE FOLDER, COMMENT IT OUT IF YOU WANT TO KEEP IT
         shutil.rmtree(transcript_path)
 
-        print("result :", result)
 
         if result.returncode != 0:
             print(f"Error transcribing audio: {result.stderr}")
